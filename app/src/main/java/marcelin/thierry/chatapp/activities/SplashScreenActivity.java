085eb24e41cb78317c8a6dc680bf8067ffea4314
Amaterasu <marcelinthierry@gmail.com>
2019-12-15 03:14:44 +0800

    // New Activity Added

---
+++ app/src/main/java/marcelin/thierry/chatapp/activities/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package marcelin.thierry.chatapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import marcelin.thierry.chatapp.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
